9cf9756c3a7b11a0b526f44ff9b68076620c304c
Pratik Patil <71940199+pratikpatil7777@users.noreply.github.com>
2022-04-02 13:13:51 -0400

    User Search

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+const jwt=require('jsonwebtoken');
 
 var firebase = require('firebase-admin')
 
 var serviceAccount = require('./service_account.json');
-// const firebaseConfig = {
+//   const firebaseConfig = {
 //   apiKey: "AIzaSyA80n1RK8xk-2PY44aZz-mY8Q8Pq3XLZm0",
 //   authDomain: "tulsee-43d3d.firebaseapp.com",
 //   projectId: "tulsee-43d3d",
@@ -20,11 +21,8 @@ firebase.initializeApp({
   databaseURL: "https://tulsee-43d3d-default-rtdb.firebaseio.com/"
 });
 
-
 const session = require('express-session');
-
 const configRoutes = require('./routes');
-
 app.use(express.json());
 
 app.use(
@@ -35,14 +33,42 @@ app.use(
         saveUninitialized: true      
     })
   );
-
 app.use(express.urlencoded({ extended: true }));
-
 configRoutes(app);
-
-
 
 app.listen(3000, () => {
   console.log("Server has been initialized!");
   console.log('Your routes will be running on http://localhost:3000');
 });
+
+/*Next function is for generating a token when user logged in.
+// generate token
+userSchema.methods.generateToken=function(cb){
+  var user =this;
+  var token=jwt.sign(user._id.toHexString(),confiq.SECRET);
+
+  user.token=token;
+  user.save(function(err,user){
+      if(err) return cb(err);
+      cb(null,user);
+  })
+}
+
+// get logged in user
+app.get('/api/profile',auth,function(req,res){
+  res.json({
+    isAuth: true,
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.firstname + req.user.lastname
+})
+
+//logout user
+app.get('/api/logout',auth,function(req,res){
+  req.user.deleteToken(req.token,(err,user)=>{
+    if(err) return res.status(400).send(err);
+    res.sendStatus(200);
+  });
+}); 
+
+*/
--- backend/data/UserRefs.js
+++ backend/data/UserRefs.js
@@ -1,15 +1,13 @@
 const { getDatabase } = require("firebase-admin/database");
 
-
+// Create a "userCollection" to access the user by it's ID from complete "users" collection
 const userCollection = userId => {
     const db = getDatabase();
     if (userId) {
         return db.ref(`server/tulsee/users/${userId}`);
     }else{
         return db.ref('server/tulsee/users');
-
     }
-    // const taskRef = ref.child('tasks')
 }
 
 module.exports = {
