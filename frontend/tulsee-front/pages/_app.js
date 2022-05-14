fb86420990b913b82c410a06b1cc20097ff6638d
Saurabh Mane <saurabhmane1997@gmail.com>
2022-05-13 23:41:34 -0400

    firebase credentails

--- frontend/tulsee-front/pages/_app.js
+++ frontend/tulsee-front/pages/_app.js
@@ -11,14 +11,22 @@ import ProjectContextProvider from '../Components/Contexts/ProjectContext';
 const MyApp = ({Component, PageProps}) => {
     console.log('firebaseId: ', process.env.FIREBASE_AUTH_DOMAIN)
     const firebaseConfig = {
-        apiKey: process.env.NEXT_PUBLIC_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        apiKey: "AIzaSyA80n1RK8xk-2PY44aZz-mY8Q8Pq3XLZm0",
+        authDomain: "tulsee-43d3d.firebaseapp.com",
+        databaseURL: "https://tulsee-43d3d-default-rtdb.firebaseio.com",
+        projectId: "tulsee-43d3d",
+        storageBucket: "tulsee-43d3d.appspot.com",
+        messagingSenderId: "357663161206",
+        appId: "1:357663161206:web:a8bafd513291667fbf1857",
+        measurementId: "G-2PQ5YJ42WS"
+        // apiKey: process.env.NEXT_PUBLIC_API_KEY,
+        // authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        // databaseURL: process.env.FIREBASE_DATABASE_URL,
+        // projectId: process.env.FIREBASE_PROJECT_ID,
+        // storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        // messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        // appId: process.env.FIREBASE_APP_ID,
+        // measurementId: process.env.FIREBASE_MEASUREMENT_ID
       };
       
       // Initialize Firebase
